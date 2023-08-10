dcbd0568faa04fba7e193eb02b07a01dcf09d937
Loaay Ahmed <141327078+LoaayAhmed@users.noreply.github.com>
2023-08-10 12:35:24 +0100

    Add files via upload

---
+++ App Pie/Game.swift
@@ -0,0 +1,35 @@
+//
+//  Game.swift
+//  App Pie
+//
+//  Created by user203868 on 7/31/23.
+//
+
+import Foundation
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+    var guessedLetters: [Character]
+    
+    mutating func playerGuessed(letter: Character) {
+        guessedLetters.append(letter)
+        if !word.contains(letter) {
+          incorrectMovesRemaining -= 1
+        }
+        
+    }
+    
+    var formattedWord: String {
+        var guessedWord = ""
+        for letter in word {
+            if guessedLetters.contains(letter) {
+                guessedWord += "\(letter)"
+            } else {
+                guessedWord += "_"
+            }
+        }
+        return guessedWord
+    }
+}
+
---
+++ App PieTests/App_PieTests.swift
@@ -0,0 +1,33 @@
+//
+//  App_PieTests.swift
+//  App PieTests
+//
+//  Created by user203868 on 7/30/23.
+//
+
+import XCTest
+@testable import App_Pie
+
+class App_PieTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
